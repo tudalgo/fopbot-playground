820633506a7e2bb789d8c140dec6d62ac0797ab0
niklhut <49069026+niklhut@users.noreply.github.com>
2024-10-28 11:44:02 +0100

    Add jitpack to gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
